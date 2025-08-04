787d857c0d26e0903d0992ce6fdba7555bc8248e
Eunsseob <163013769+Eunsseob@users.noreply.github.com>
2025-08-04 17:22:39 +0900

    feat : 25.08.04

---
+++ chap01/src/_02_print/T01_print_println.java
@@ -0,0 +1,8 @@
+package _02_print;
+
+public class T01_print_println {
+
+	public static void main(String[] args) {
+		System.out.println("test" + "\n내이름은");
+	}
+}
---
+++ chap01/src/_02_print/T02_println_String.java
@@ -0,0 +1,21 @@
+package _02_print;
+
+import java.util.*;
+
+public class T02_println_String {
+
+	public static void main(String[] args){
+	       
+        Scanner sc = new Scanner(System.in);
+        
+        int m;
+        
+        int K = sc.nextInt();
+        
+        for(m = 0; 0 < K; K--){
+            m = m+K;
+        }
+        
+        System.out.print(m);
+    }
+}
